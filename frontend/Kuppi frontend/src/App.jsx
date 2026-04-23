67351d8ac223c55065c0f435d4422ec16f715145
it24101871 <it24101871@my.sliit.lk>
2026-04-23 16:33:27 +0530

    Mern Item Manager Project

---
+++ frontend/Kuppi frontend/src/App.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import { getItems } from './api';
+import ItemForm from './ItemForm';
+import ItemList from './ItemList';
+
+function App() {
+ const [items, setItems] = useState([]);
+
+const fetchItems = async () => {
+  const res = await getItems();
+  console.log("Fetched:", res.data); // debug
+  setItems(Array.isArray(res.data) ? res.data : []);
+
+ };
+ useEffect(() => {
+  fetchItems();
+ }, []);
+
+ return (
+  <div style={{ maxWidth: '700px', margin: '2rem auto', fontFamily: 'sans-serif' }}>
+
+    <h1>Item Manager</h1>
+    <ItemForm onItemAdded={fetchItems} />
+    <ItemList items={items} onRefresh={fetchItems} />
+  </div>
+ );
+}
+export default App;
